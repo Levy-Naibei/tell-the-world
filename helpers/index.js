bd546282d339853198a20ee8f11bdad42e72bd60
Levy-Naibei <levynaibei2@gmail.com>
2020-08-27 23:37:56 +0300

    feat: truncate characters, strip html tags

--- helpers/index.js
+++ helpers/index.js
@@ -3,5 +3,20 @@ const moment = require('moment');
 module.exports = {
     formatDate: (date, format) => {
         return moment(date).format(format)
+    },
+
+    truncate: (str, len) => {
+        if (str.length > len && str.length > 0){
+            let new_str = str + ''
+            new_str = str.substr(0, len)
+            new_str = str.substr(0, new_str.lastIndexOf(''))
+            new_str = str.length > 0 ? new_str:str.substr(0, len)
+            return new_str + '';
+        }
+        return new_str;
+    },
+
+    stripTags: (input) => {
+        return input.replace(/<(?:.|\n)*?>/gm, '');
     }
 }
